e72591e6bbf578708bb5d8c649a0302923d2cecc
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-27 11:44:42 +0100

    in the beniining

--- Resonance/Program.cs
+++ Resonance/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Resonance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Resonance");
+
+        AudioFormat format = AudioFormat.CDMono;
+
+        // new SoundEffect(format) would be cool
+        // then just SaveSound(sound.Generate(length), name)
+    }
+
+    static void SaveSound(AudioBuffer buffer, string fileName)
+    {
+        byte[] data = buffer.ToWavBytes();
+        File.WriteAllBytes(fileName, data);
+    }
+}
