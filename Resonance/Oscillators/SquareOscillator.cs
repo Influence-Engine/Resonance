4a784fa17e73f51f4ab9bd2c9793943371b7233a
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-27 10:19:37 +0100

    square up

---
+++ Resonance/Oscillators/SquareOscillator.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Resonance.Oscillators
+{
+    /// <summary>Square wave oscillator</summary>
+    public class SquareOscillator : OscillatorBase
+    {
+        float _pulseWidth = 0.5f;
+        public float PulseWidth
+        {
+            get => _pulseWidth;
+            set => _pulseWidth = Math.Clamp(value, 0.01f, 0.99f);
+        }
+
+        public SquareOscillator(AudioFormat format) : base(format) { }
+
+        public override float NextSample()
+        {
+            float sample = phase < _pulseWidth ? 1f : -1f;
+            AdvancePhase();
+
+            return sample;
+        }
+    }
+}
