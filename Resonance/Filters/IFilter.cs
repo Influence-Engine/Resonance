f40f91521d1011461b342e2f0dff9711e42214c9
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-27 14:01:23 +0100

    Filters!!! WAAAA

---
+++ Resonance/Filters/IFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Resonance.Filters
+{
+    public enum FilterType
+    {
+        LowPass,
+        HighPass,
+        BandPass,
+        // Let me google more later lol
+    }
+
+    public interface IFilter
+    {
+        FilterType Type { get; }
+        int SampleRate { get; }
+
+        float Process(float input);
+
+        void ProcessBlock(Span<float> samples);
+
+        void Reset();
+    }
+}
