61c86d9381f433e405d91274bfef772c700ce7b3
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-27 11:19:46 +0100

    white noise to sleep to

---
+++ Resonance/Oscillators/NoiseOscillator.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Resonance.Oscillators
+{
+    /// <summary>Its just white noise</summary>
+    public class NoiseOscillator : OscillatorBase
+    {
+        readonly Random random;
+
+        public NoiseOscillator(AudioFormat format, int? seed = null) : base(format)
+        {
+            this.random = seed.HasValue ? new Random(seed.Value) : new Random();
+        }
+
+        public override float NextSample() => (float)(random.NextDouble() * 2.0 - 1.0);
+
+        // These don't do anything (I think)
+        public override void SetFrequency(float frequencyHz) { }
+        public override void Reset() { }
+    }
+}
