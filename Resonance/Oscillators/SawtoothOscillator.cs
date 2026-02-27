5a180c21fb14b6c7c04acd3fb9243ac9e4f6f507
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-27 11:08:56 +0100

    sawtooth

---
+++ Resonance/Oscillators/SawtoothOscillator.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Resonance.Oscillators
+{
+    /// <summary>Sawtooth wave oscillator</summary>
+    public class SawtoothOscillator : OscillatorBase
+    {
+        public SawtoothOscillator(AudioFormat format) : base(format) { }
+
+        public override float NextSample()
+        {
+            float sample = 2f * phase - 1f;
+            AdvancePhase();
+
+            return sample;
+        }
+    }
+}
