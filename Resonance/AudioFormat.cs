f30495f94ba474ac5389a05793242cd10ef96b71
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-27 09:23:31 +0100

    is this right?

---
+++ Resonance/AudioFormat.cs
@@ -0,0 +1,27 @@
+﻿namespace Resonance
+{
+    public class AudioFormat
+    {
+        public int SampleRate { get; }
+        public int Channels { get; }
+        public int BytesPerSample => 2; // 16-bit PCM
+
+        public AudioFormat(int sampleRate = 44100, int channels = 1)
+        {
+            if (sampleRate <= 0)
+                throw new ArgumentException("Sample rate must be positive");
+
+            if (channels <= 0)
+                throw new ArgumentException("Channels must be positive");
+
+            this.SampleRate = sampleRate;
+            this.Channels = channels;
+        }
+
+        public int SecondsToSamples(float seconds) => (int)(seconds * SampleRate);
+
+        public float SamplesToSeconds(int samples) => samples / (float)SampleRate;
+
+        public float FrequencyToPhaseIncrement(float frequencyHz) => frequencyHz / SampleRate;
+    }
+}
