f1bfbd51f41728cc26ed22bc7733ef8738682886
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-27 11:16:49 +0100

    Triangles have 3 corners :)

---
+++ Resonance/Oscillators/TriangleOscillator.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Resonance.Oscillators
+{
+    /// <summary>Triangle wave oscillator</summary>
+    public class TriangleOscillator : OscillatorBase
+    {
+        public TriangleOscillator(AudioFormat format) : base(format) { }
+
+        public override float NextSample()
+        {
+            // Rising: -1 to 1
+            // Falling: 1 to -1
+            float sample = phase < 0.5f ? 4f * phase - 1f : 3f - 4f * phase;
+            AdvancePhase();
+
+            return sample;
+        }
+    }
+}
