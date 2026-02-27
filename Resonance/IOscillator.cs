57ca9d9a3f97daeda6621890ea9dc73d8b1a0165
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-27 10:11:28 +0100

    dont mind me oscillating <.<

---
+++ Resonance/IOscillator.cs
@@ -0,0 +1,12 @@
+﻿namespace Resonance
+{
+    // Petition to call it IResonator >.<
+    public interface IOscillator
+    {
+        float NextSample();
+
+        void SetFrequency(float frequencyHz);
+
+        void Reset();
+    }
+}
