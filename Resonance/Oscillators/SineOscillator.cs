5ad92aa96dfcd3075bf388c5dbf0f241092591db
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-27 11:13:18 +0100

    sine

---
+++ Resonance/Oscillators/SineOscillator.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Resonance.Oscillators
+{
+    /// <summary>Sine wave oscillator</summary>
+    public class SineOscillator : OscillatorBase
+    {
+        public SineOscillator(AudioFormat format) : base(format) { }
+
+        public override float NextSample()
+        {
+            float sample = (float)Math.Sin(phase * 2.0 * Math.PI);
+            AdvancePhase();
+
+            return sample;
+        }
+    }
+}
